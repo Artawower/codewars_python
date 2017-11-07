2e89a7a05efb2bdce64b7e78e61db7a4dc9b3134
artawower <artawower@mail.ru>
2017-11-07 19:29:39 +0300

    add +1 task

--- 8kye5.py
+++ 8kye5.py
@@ -1,3 +1,10 @@
+#              1
+#           3     5
+#        7     9    11
+#    13    15    17    19
+# 21    23    25    27    29
+# ...
+
 def row_sum_odd_numbers(n):
 	first_symbol = 1
 	for i in range(n - 1):
